f90da2b92dff1d65b3f34e01b847fd8c49045fae
Hossein Douzandeh <douzandeh.k@gmail.com>
2025-12-27 03:35:33 +0330

    feat: initialize app layout with navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
-    <div>
-      {" "}
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="overflow-x-hidden text-stone-300 antialiased">
+      <div className="fixed inset-0 -z-10">
+        <div className="relative h-full w-full bg-slate-950">
+          <div className="absolute bottom-0 left-[-20%] right-0 top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div>
+          <div className="absolute bottom-0 right-[-20%] top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div>
+        </div>
+      </div>
+      <div className="container mx-auto px-8">
+        <Navbar />
+      </div>
     </div>
   );
 }
