ddc65d9ced7503fd3e08a926ec09ed52a8f56ae7
Hossein Douzandeh <douzandeh.k@gmail.com>
2025-12-29 00:25:08 +0330

    feat: add constants for hero section for HERO_CONTENT

---
+++ src/constants/index.js
@@ -0,0 +1,6 @@
+import project1 from "../assets/projects/project-1.webp";
+import project2 from "../assets/projects/project-2.webp";
+import project3 from "../assets/projects/project-3.webp";
+import project4 from "../assets/projects/project-4.webp";
+
+export const HERO_CONTENT = `I am a front-end developer focused on building modern, responsive, and user-friendly web interfaces. I work with HTML, CSS, JavaScript, and React, and I am currently learning Next.js to create fast and scalable web applications. I enjoy translating designs into clean, maintainable code and continuously improving my skills through real-world projects.`;
